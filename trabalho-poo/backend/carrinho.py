23ea33ad0d1c264300a99e9396d7dd059d23294e
RafaelRodrigues87 <rafaelrodigues87@gmail.com>
2024-11-05 19:57:04 -0300

    primeiro commit

---
+++ trabalho-poo/backend/carrinho.py
@@ -0,0 +1,2 @@
+class carinho:
+    def __init__(self)
---
+++ trabalho-poo/backend/produto.py
@@ -0,0 +1,9 @@
+class produto:
+    def __init__(self, produto_nome, codproduto, preco, descricao, produto_quantidade):
+        self.codproduto = codproduto
+        self.preco = preco
+        self.produto_quantidade = produto_quantidade
+        self.descricao = descricao
+        self.produto_nome = produto_nome
+        
+    
